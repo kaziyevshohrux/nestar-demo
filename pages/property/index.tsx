8a509e4f7de9a7c8d256cc81de51321955df927a
OMAR <OMAR@comets.com>
2026-09-14 02:39:00 +0900

    feat: publish propert list

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,12 +1,55 @@
-import withLayoutBasic from "@/libs/components/layout/LayoutBasic"
-import { Container } from "@mui/material"
-import { NextPage } from "next"
+import React, { useState } from "react";
+import { NextPage } from "next";
+import { Box, Button, Pagination, Stack, Typography } from "@mui/material";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import Filter from "./Filter";
+import PropertyCard from "./PropertyCard";
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
 
 const PropertyList: NextPage = () => {
-    return (
-    <>
-    <Container>Property list</Container>
-    </>
-)}
+  const [properties, setProperties] = useState<number[]>([1, 2, 3, 4, 5, 6]);
 
-export default withLayoutBasic(PropertyList)
+  return (
+    <div id={"property-list-page"} style={{ position: "relative" }}>
+      <Stack className={"container"}>
+        <Box className={"right"}>
+          <span>Sort by</span>
+          <div>
+            <Button endIcon={<KeyboardArrowDownRoundedIcon />}>New</Button>
+          </div>
+        </Box>
+
+        <Stack className={"property-page"}>
+          <Stack className={"filter-config"}>
+            <Filter />
+          </Stack>
+
+          <Stack className={"main-config"} mb={"76px"}>
+            <Stack className={"list-config"}>
+              {properties.map((property, index) => {
+                return <PropertyCard key={index} />;
+              })}
+            </Stack>
+
+            <Stack className={"pagination-config"}>
+              <Stack className="pagination-box">
+                <Pagination
+                  page={1}
+                  count={5}
+                  shape="circular"
+                  color="primary"
+                />
+              </Stack>
+
+              <Stack className="total-result">
+                <Typography>Total 5 properties available</Typography>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Stack>
+    </div>
+  );
+};
+
+export default withLayoutBasic(PropertyList);
